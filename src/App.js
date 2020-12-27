387f2f281cc5f884a07be5eb0a4d588f88a458e3
achyuth reddy <ncsachyuth@gmail.com>
2020-12-27 13:50:32 +0530

    added the router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import logo from './logo.svg'
 import './App.css'
+import { Switch, Route } from 'react-router-dom'
 
 function App () {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        <h1>achyuth</h1>
-        <h2>reddy checking</h2>
-        <h1>how about now ???</h1>
-      </header>
+      <Switch>
+        <Route exact={true} path='/' component={HomePage} />
+        <Route exact={true} path='/dashboard' component={dashboard} />
+      </Switch>
     </div>
   )
 }
 
+function HomePage () {
+  return (
+    <div className=''>
+      <h1>home page</h1>
+    </div>
+  )
+}
+function dashboard () {
+  return (
+    <div className=''>
+      <h1>dashboard page</h1>
+    </div>
+  )
+}
 export default App
